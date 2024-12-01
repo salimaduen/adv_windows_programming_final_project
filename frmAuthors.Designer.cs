ad27f95b648a674f09470a5417fb478df3e10993
salim <salimaduen@hotmail.com>
2024-12-01 14:13:07 -0500

    draft for add/remove

--- frmAuthors.Designer.cs
+++ frmAuthors.Designer.cs
@@ -31,12 +31,11 @@
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.txtContract = new System.Windows.Forms.TextBox();
             this.txtState = new System.Windows.Forms.TextBox();
             this.txtCity = new System.Windows.Forms.TextBox();
-            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
             this.txtLName = new System.Windows.Forms.TextBox();
-            this.txtTitleID = new System.Windows.Forms.TextBox();
+            this.txtFname = new System.Windows.Forms.TextBox();
             this.lblState = new System.Windows.Forms.Label();
             this.lblZip = new System.Windows.Forms.Label();
             this.lblContract = new System.Windows.Forms.Label();
@@ -51,93 +50,87 @@
             this.txtZip = new System.Windows.Forms.MaskedTextBox();
             this.txtId = new System.Windows.Forms.MaskedTextBox();
             this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.cmbContract = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnRemove
             // 
             this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRemove.Location = new System.Drawing.Point(1187, 448);
-            this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(133, 38);
             this.btnRemove.TabIndex = 83;
             this.btnRemove.Text = "Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAdd.Location = new System.Drawing.Point(1024, 448);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(133, 38);
             this.btnAdd.TabIndex = 82;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.Location = new System.Drawing.Point(1187, 15);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(133, 38);
             this.btnExit.TabIndex = 81;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             // 
-            // txtContract
-            // 
-            this.txtContract.Location = new System.Drawing.Point(1212, 575);
-            this.txtContract.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtContract.MaxLength = 1;
-            this.txtContract.Name = "txtContract";
-            this.txtContract.Size = new System.Drawing.Size(132, 22);
-            this.txtContract.TabIndex = 79;
-            // 
             // txtState
             // 
             this.txtState.Location = new System.Drawing.Point(929, 575);
-            this.txtState.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtState.Margin = new System.Windows.Forms.Padding(4);
             this.txtState.Name = "txtState";
             this.txtState.Size = new System.Drawing.Size(132, 22);
-            this.txtState.TabIndex = 77;
+            this.txtState.TabIndex = 8;
             // 
             // txtCity
             // 
             this.txtCity.Location = new System.Drawing.Point(788, 575);
-            this.txtCity.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(4);
             this.txtCity.MaxLength = 20;
             this.txtCity.Name = "txtCity";
             this.txtCity.Size = new System.Drawing.Size(132, 22);
-            this.txtCity.TabIndex = 76;
+            this.txtCity.TabIndex = 7;
             // 
-            // txtPrice
+            // txtAddress
             // 
-            this.txtPrice.Location = new System.Drawing.Point(647, 575);
-            this.txtPrice.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtPrice.MaxLength = 40;
-            this.txtPrice.Name = "txtPrice";
-            this.txtPrice.Size = new System.Drawing.Size(132, 22);
-            this.txtPrice.TabIndex = 75;
+            this.txtAddress.Location = new System.Drawing.Point(647, 575);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAddress.MaxLength = 40;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(132, 22);
+            this.txtAddress.TabIndex = 6;
             // 
             // txtLName
             // 
             this.txtLName.Location = new System.Drawing.Point(364, 575);
-            this.txtLName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtLName.Margin = new System.Windows.Forms.Padding(4);
             this.txtLName.MaxLength = 40;
             this.txtLName.Name = "txtLName";
             this.txtLName.Size = new System.Drawing.Size(132, 22);
-            this.txtLName.TabIndex = 73;
+            this.txtLName.TabIndex = 4;
             // 
-            // txtTitleID
+            // txtFname
             // 
-            this.txtTitleID.Location = new System.Drawing.Point(223, 575);
-            this.txtTitleID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtTitleID.MaxLength = 20;
-            this.txtTitleID.Name = "txtTitleID";
-            this.txtTitleID.Size = new System.Drawing.Size(132, 22);
-            this.txtTitleID.TabIndex = 72;
+            this.txtFname.Location = new System.Drawing.Point(223, 575);
+            this.txtFname.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFname.MaxLength = 20;
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(132, 22);
+            this.txtFname.TabIndex = 3;
             // 
             // lblState
             // 
@@ -241,10 +234,10 @@
             // txtSearchBar
             // 
             this.txtSearchBar.Location = new System.Drawing.Point(81, 30);
-            this.txtSearchBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearchBar.Margin = new System.Windows.Forms.Padding(4);
             this.txtSearchBar.Name = "txtSearchBar";
             this.txtSearchBar.Size = new System.Drawing.Size(404, 22);
-            this.txtSearchBar.TabIndex = 60;
+            this.txtSearchBar.TabIndex = 1;
             this.txtSearchBar.Text = "Search author name...";
             this.txtSearchBar.TextChanged += new System.EventHandler(this.txtSearchBar_TextChanged);
             // 
@@ -253,56 +246,68 @@
             this.lstAuthors.FormattingEnabled = true;
             this.lstAuthors.ItemHeight = 16;
             this.lstAuthors.Location = new System.Drawing.Point(81, 66);
-            this.lstAuthors.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstAuthors.Margin = new System.Windows.Forms.Padding(4);
             this.lstAuthors.Name = "lstAuthors";
             this.lstAuthors.ScrollAlwaysVisible = true;
             this.lstAuthors.Size = new System.Drawing.Size(1237, 356);
             this.lstAuthors.TabIndex = 59;
+            this.lstAuthors.SelectedIndexChanged += new System.EventHandler(this.lstAuthors_SelectedIndexChanged);
             // 
             // txtZip
             // 
             this.txtZip.Location = new System.Drawing.Point(1071, 575);
-            this.txtZip.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtZip.Margin = new System.Windows.Forms.Padding(4);
             this.txtZip.Mask = "00000";
             this.txtZip.Name = "txtZip";
             this.txtZip.Size = new System.Drawing.Size(132, 22);
-            this.txtZip.TabIndex = 84;
+            this.txtZip.TabIndex = 9;
             // 
             // txtId
             // 
             this.txtId.Location = new System.Drawing.Point(81, 575);
-            this.txtId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtId.Mask = "000000000";
+            this.txtId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtId.Mask = "000-00-0000";
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(132, 22);
-            this.txtId.TabIndex = 85;
+            this.txtId.TabIndex = 2;
             // 
             // txtPhone
             // 
             this.txtPhone.Location = new System.Drawing.Point(505, 575);
-            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtPhone.Mask = "(999) 000-0000";
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPhone.Mask = "999-000-0000";
             this.txtPhone.Name = "txtPhone";
             this.txtPhone.Size = new System.Drawing.Size(132, 22);
-            this.txtPhone.TabIndex = 86;
+            this.txtPhone.TabIndex = 5;
+            // 
+            // cmbContract
+            // 
+            this.cmbContract.FormattingEnabled = true;
+            this.cmbContract.Items.AddRange(new object[] {
+            "True",
+            "False"});
+            this.cmbContract.Location = new System.Drawing.Point(1212, 575);
+            this.cmbContract.Name = "cmbContract";
+            this.cmbContract.Size = new System.Drawing.Size(121, 24);
+            this.cmbContract.TabIndex = 10;
             // 
             // frmAuthors
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1440, 785);
+            this.Controls.Add(this.cmbContract);
             this.Controls.Add(this.txtPhone);
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.txtZip);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.txtContract);
             this.Controls.Add(this.txtState);
             this.Controls.Add(this.txtCity);
-            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.txtLName);
-            this.Controls.Add(this.txtTitleID);
+            this.Controls.Add(this.txtFname);
             this.Controls.Add(this.lblState);
             this.Controls.Add(this.lblZip);
             this.Controls.Add(this.lblContract);
@@ -315,7 +320,7 @@
             this.Controls.Add(this.txtSearchBar);
             this.Controls.Add(this.lstAuthors);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAuthors";
             this.Text = "Author";
             this.ResumeLayout(false);
@@ -328,12 +333,11 @@
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.TextBox txtContract;
         private System.Windows.Forms.TextBox txtState;
         private System.Windows.Forms.TextBox txtCity;
-        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.TextBox txtLName;
-        private System.Windows.Forms.TextBox txtTitleID;
+        private System.Windows.Forms.TextBox txtFname;
         private System.Windows.Forms.Label lblState;
         private System.Windows.Forms.Label lblZip;
         private System.Windows.Forms.Label lblContract;
@@ -348,5 +352,6 @@
         private System.Windows.Forms.MaskedTextBox txtZip;
         private System.Windows.Forms.MaskedTextBox txtId;
         private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.ComboBox cmbContract;
     }
 }
--- frmAuthors.cs
+++ frmAuthors.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -38,12 +40,12 @@ namespace Final_Project
                     using (BookStoreEntities context = new BookStoreEntities())
                     {
                         var authors = context.authors
-                            .Select(a => new {a.au_fname, a.au_lname})
+                            .Select(a => new {a.au_fname, a.au_lname, a.au_id})
                             .ToList();
 
                         foreach (var auth in authors)
                         {
-                            this.lstAuthors.Items.Add($"{auth.au_fname} {auth.au_lname}");
+                            this.lstAuthors.Items.Add($"{auth.au_fname} {auth.au_lname} {auth.au_id}");
                         }
                     }
                 }
@@ -57,14 +59,14 @@ namespace Final_Project
 
                     var authors = context.authors
                         .Where(a => a.au_fname.ToLower().StartsWith(searchText) || a.au_lname.ToLower().StartsWith(searchText))
-                        .Select(a => new { a.au_fname, a.au_lname })
+                        .Select(a => new { a.au_fname, a.au_lname, a.au_id})
                         .ToList();
 
                     if (authors.Count > 0)
                     {
                         foreach (var auth in authors)
                         {
-                            this.lstAuthors.Items.Add($"{auth.au_fname} {auth.au_lname}");
+                            this.lstAuthors.Items.Add($"{auth.au_fname} {auth.au_lname} {auth.au_id}");
                         }
                     } else
                     {
@@ -74,5 +76,164 @@ namespace Final_Project
                 }
             }
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text.Trim();
+            string fName = txtFname.Text.Trim();
+            string lName = txtLName.Text.Trim();
+            string phone = Regex.Replace(txtPhone.Text.Trim(), @"[()\\s]", "");
+            string address = txtAddress.Text.Trim();
+            string city = txtCity.Text.Trim();
+            string state = txtState.Text.Trim().ToUpper();
+            string zip = txtZip.Text.Trim();
+            string contractText = cmbContract.Text.Trim();
+
+
+            using (BookStoreEntities context = new BookStoreEntities())
+            {
+                try
+                {
+   
+                    bool authorExists = context.authors.Any(a => a.au_id == id);
+
+                    if (authorExists)
+                    {
+                        MessageBox.Show("Author already exists!", "Duplicate Record", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    
+                    var newAuthor = new author
+                    {
+                        au_id = id,
+                        au_fname = fName,
+                        au_lname = lName,
+                        phone = phone,
+                        address = string.IsNullOrEmpty(address) ? null : address,
+                        city = string.IsNullOrEmpty(city) ? null : city,
+                        state = string.IsNullOrEmpty(state) ? null : state,
+                        zip = zip,
+                        contract = Boolean.Parse(contractText)
+                    };
+                    
+                    context.authors.Add(newAuthor);
+                    context.SaveChanges();
+                    txtSearchBar_TextChanged(sender, e);
+                    MessageBox.Show("Author added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    var innerException = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                    MessageBox.Show($"An error occurred while adding the author:\n{innerException}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void lstAuthors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstAuthors.SelectedIndex == -1)
+                return;
+
+            string selected = lstAuthors.Items[lstAuthors.SelectedIndex].ToString();
+            string[] selectedId = selected.Split(' ');
+            selected = selectedId[selectedId.Length - 1];
+
+            if (!string.IsNullOrEmpty(selected))
+            {
+                try
+                {
+                    using (BookStoreEntities context = new BookStoreEntities())
+                    {
+                        var author = context.authors
+                            .Where(a => a.au_id.Equals(selected))
+                            .FirstOrDefault();
+
+                        if (author != null)
+                        {
+                            txtId.Text = author.au_id;
+                            txtFname.Text = author.au_fname;
+                            txtLName.Text = author.au_lname;
+                            txtPhone.Text = author.phone.Replace(" ", "").Replace("-", "");
+                            txtAddress.Text = author.address;
+                            txtCity.Text = author.city;
+                            txtState.Text = author.state;
+                            txtZip.Text = author.zip;
+                            cmbContract.Text = author.contract.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No author found with the selected ID.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error occurred while fetching the author:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text.Trim();
+
+         
+            if (string.IsNullOrEmpty(id))
+            {
+                MessageBox.Show("Please enter a valid ID to remove.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (BookStoreEntities context = new BookStoreEntities())
+            {
+                try
+                {
+                    var authorToRemove = context.authors.SingleOrDefault(a => a.au_id == id);
+
+                    if (authorToRemove == null)
+                    {
+                        MessageBox.Show("Author not found!", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    var relatedRecords = context.titleauthors.Where(ta => ta.au_id == id).ToList();
+                    if (relatedRecords.Any())
+                    {
+                        context.titleauthors.RemoveRange(relatedRecords);
+                    }
+
+                    context.authors.Remove(authorToRemove);
+
+                    context.SaveChanges();
+
+                    MessageBox.Show("Author record removed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    ClearInputFields();
+                    txtSearchBar_TextChanged(sender, e);
+                }
+                catch (Exception ex)
+                {
+                    var innerException = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                    MessageBox.Show($"Error occurred while removing the author:\n{innerException}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void ClearInputFields()
+        {
+            txtId.Text = "";
+            txtFname.Text = "";
+            txtLName.Text = "";
+            txtPhone.Text = "";
+            txtAddress.Text = "";
+            txtCity.Text = "";
+            txtState.Text = "";
+            txtZip.Text = "";
+            cmbContract.SelectedIndex = -1;
+        }
+
     }
 }
